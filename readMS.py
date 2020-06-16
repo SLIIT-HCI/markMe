16c5681ecb6dc52bfdbb4352132e98bc62cfe2cd
Thusithanjana Thilakarathana <50567237+Thusithanjana@users.noreply.github.com>
2020-06-16 07:43:02 +0530

    Updated with comments

--- readMS.py
+++ readMS.py
@@ -1,22 +1,28 @@
+#this contains the readMS class
+
 import re
 class readMS:    
 
     def __init__(self,question):
         self.q = question
 
+    # split the classes in the Marking scheme in order to identify the marking distribution accross sections
     def classSplit(self):
         file = open(self.q)
         r = file.read()
 
         return r.strip().split("#")
 
+    #split the file into lines
     def lineSplit(self,cls):    
 
         return cls.split('\n')
 
+    #remove special charactors
     def removeAlpha(self,line):
         return re.findall(r"[-+]?\d*\.\d+|\d+", line)
 
+    #return mark allocations in each section
     def getMsMarks(self,classes):
         
         marks = []
